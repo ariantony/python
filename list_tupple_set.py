fdeb5f4672c2069bb4761c5e105aceb88d5dca68
tony arianto <ariantony@gmail.com>
2017-09-10 21:11:18 +0700

    menambahkan file list_tupple_set

---
+++ list_tupple_set.py
@@ -0,0 +1,7 @@
+grade_list = [9,9,9,10]
+grade_tupple = (1,2,3,4,5,6,7,8,9,9,9,9)
+grade_set = {1,2,3,4,5,6,7,8,9,9,9}
+
+grade_list.append(100)
+print(grade_list)
+print(sum(grade_list)/len(grade_list))
